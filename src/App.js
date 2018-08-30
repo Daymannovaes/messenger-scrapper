3231ce7cb5f7be39f8a69ffbfc6300f47cecbde0
Dayman Novaes <daymannovaes@gmail.com>
2018-08-30 20:45:36 -0300

    add styled input

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Input from './components/Input';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to my Messenger Scrapper</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Input placeholder="this is an input" />
       </div>
     );
   }
---
+++ src/components/Input.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default styled.input`
+    padding: 6px 12px;
+    border-radius: 3px;
+    border: 2px solid #ccc;
+
+    transition: 200ms all;
+
+    &:hover {
+        border-color: #999;
+    }
+`;
