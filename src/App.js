e4fba86dc9d11faa0c86cf78b72530f8a41042f2
Dayman Novaes <daymannovaes@gmail.com>
2018-08-30 21:43:51 -0300

    add component: LocalStorageInput

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Input from './components/Input';
+import LocalStorageInput from './components/LocalStorageInput';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    handleChange = event => {
+        this.setState({ value: event.target.value });
+    }
   render() {
     return (
       <div className="App">
@@ -12,7 +19,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to my Messenger Scrapper</h1>
         </header>
-        <Input placeholder="this is an input" />
+        <LocalStorageInput name="localStorageTest" placeholder="this is an input" onChange={this.handleChange} />
+
+        {this.state.value}
       </div>
     );
   }
---
+++ src/components/LocalStorageInput.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Input from './Input';
+
+export default class LocalStorageInput extends Component {
+    constructor(props) {
+        super(props);
+
+        this.STORAGE_NAME = `components.${props.name}`;
+
+        this.state = {
+            value: this.getLocalStorageValue() || props.initialValue || ''
+        };
+
+        this.handleChange({ target: { value: this.state.value } });
+    }
+
+    getLocalStorageValue = () => localStorage.getItem(this.STORAGE_NAME)
+
+    handleChange = event => {
+        const { value } = event.target;
+
+        this.setState({ value });
+        localStorage.setItem(this.STORAGE_NAME, value);
+
+        this.props.onChange && this.props.onChange(event);
+    }
+
+    render() {
+        return <Input {...this.props} value={this.state.value} name={this.props.name} onChange={this.handleChange} />;
+    }
+}
+
