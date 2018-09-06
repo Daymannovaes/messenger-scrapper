9200a944f134e36f6d05b0dce7d658da3f263aa9
Dayman Novaes <daymannovaes@gmail.com>
2018-09-05 22:10:59 -0300

    add cors in _server

--- _server/handler.js
+++ _server/handler.js
@@ -5,22 +5,23 @@ module.exports.fetch = async (event, context) => {
     const queryStringParameters = event.queryStringParameters || {};
     const { before, cookies, userId } = queryStringParameters;
 
-    if(!cookies || !userId) return {
-        statusCode: 400,
-        body: JSON.stringify({
+    if(!cookies || !userId) {
+        return Response(400, {
             message: "missing cookies or userId as parameter in query string"
-        })
-    };
+        });
+    }
 
     return fetchMessagesBefore({ before, cookies, userId })
-    .then(res => ({
-        statusCode: 200,
-        body: JSON.stringify(res)
-    }))
-    .catch(error => ({
-        statusCode: 400,
-        body: JSON.stringify({
-            message: error.toString(),
-        })
+    .then(res => Response(200, res))
+    .catch(error => Response(400, {
+        message: error.toString(),
     }));
 };
+
+const Response = (statusCode, body) => ({
+    statusCode,
+    headers: {
+        "Access-Control-Allow-Origin" : "*",
+    },
+    body: JSON.stringify(body)
+});
